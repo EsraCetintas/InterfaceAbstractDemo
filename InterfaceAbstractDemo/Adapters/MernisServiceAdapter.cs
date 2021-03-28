cd077aa0f997f416b6963d50cd994908b1ee95a3
esra.cetintas@outlook.com.tr <esra.cetintas@outlook.com.tr>
2021-03-28 21:16:50 +0300

    Proje dosyası ekle.

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient kPSPublicSoapClient = new KPSPublicSoapClient();
+           return kPSPublicSoapClient.TCKimlikNoDogrula(Convert.ToInt64(customer.NationalyId),
+               customer.FirstName.ToUpper(),customer.LastName.ToUpper(),customer.DateOfBirth.Year);
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager baseCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+           //Kimlik bilgilerinizle test ediniz
+            baseCustomerManager.Save(new Customer { DateOfBirth = new DateTime(), FirstName = "", LastName = "", NationalyId = "" });
+            Console.ReadKey();
+        }
+    }
+}
